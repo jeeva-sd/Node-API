8570153442357b5abc54f242d58ac39b0bc2688f
jeeva <jeevasubash64@gmail.com>
2024-01-17 18:14:51 +0530

    validateParams fixes.

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -6,7 +6,7 @@ import { MiddlewareFunction } from './type';
 export const validateParams = (schema: yup.AnyObjectSchema): MiddlewareFunction => {
     return async (req: RequestX, res: Response, next: NextFunction): Promise<void> => {
         try {
-            const params = { ...req.parameters, ...req.params, ...req.query };
+            const params = { ...req.body, ...req.params, ...req.query };
             const validatedParams = await schema.validate(params, { abortEarly: false });
             req.parameters = validatedParams;
             next();
